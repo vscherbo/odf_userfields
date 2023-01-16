0e125412f76bdb841fafb5b04f9ebfeb45964ddd
Vladimir Scherbo <vscherbo@gmail.com>
2023-01-16 17:25:05 +0300

    recovered from DB

--- sql/cr-bill_fax_odf.sql
+++ sql/cr-bill_fax_odf.sql
@@ -1,6 +1,4 @@
--- DROP FUNCTION rep.bill_fax_odf(arg_bill_no integer);
-
-CREATE OR REPLACE FUNCTION rep.bill_fax_odf(arg_bill_no integer, arg_templ varchar DEFAULT 'СчетФакс')
+CREATE OR REPLACE FUNCTION rep.bill_fax_odf(arg_bill_no integer, arg_templ character varying DEFAULT 'СчетФакс'::character varying)
  RETURNS character varying
  LANGUAGE plpgsql
 AS $function$                                                                           
@@ -29,9 +27,10 @@ BEGIN
 
     loc_res := rep.set_userfields_common(format('%s/bill_fax_template.odt', templ_dir),
                                          format(E'%s/%s', out_dir, out_file), 
-                                         format('select * from bill_fax_data (%s)
+                                         format('select * from bill_fax_data (%s, %s)
 AS (
 pg_firm TEXT
+, pg_position text
 , pg_signature text
 , pg_proxy_doc text
 , pg_firm_full_name text
@@ -72,14 +71,19 @@ pg_firm TEXT
 , pg_logo text
 , pg_firm_big text
 );'
-, arg_bill_no));
-    RAISE NOTICE 'set_userfields_common loc_res=%', loc_res;
-    if loc_res <> '' then res := concat_ws(E'/', res, loc_res); end if;
+, arg_bill_no, quote_literal(arg_templ)));
+    if loc_res <> '' then
+        res := concat_ws(E'/', res, loc_res);
+        RAISE NOTICE 'set_userfields_common loc_res=%', loc_res;
+    end if;
 
     -- table
+    RAISE NOTICE 'start fill_table_items arg_bill_no=%', arg_bill_no;
     loc_res := rep.fill_table_items(arg_bill_no, out_dir, out_file);
-    RAISE NOTICE 'fill_table_items loc_res=%', loc_res;
-    if loc_res <> '' then res := concat_ws(E'/', res, loc_res); end if;
+    if loc_res <> '' then 
+        res := concat_ws(E'/', res, loc_res); 
+        RAISE NOTICE 'fill_table_items loc_res=%', loc_res;
+    end if;
 
     -- our firm
     SELECT b."фирма" INTO our_firm
@@ -97,26 +101,34 @@ pg_firm TEXT
     if bill_logo_file IS NOT NULL then
         loc_res := rep.replace_image_common(format('%s/%s', out_dir, out_file), 
                                                   format('%s/%s', templ_dir, bill_logo_file), 'bill_logo');
-        RAISE NOTICE 'LOGO replace_image_common loc_res=%', loc_res;
-        if loc_res <> '' then res := concat_ws(E'/', res, loc_res); end if;
+        if loc_res <> '' then
+            res := concat_ws(E'/', res, loc_res);
+            RAISE NOTICE 'LOGO replace_image_common loc_res=%', loc_res;
+        end if;
     end if;                                    
 
     -- stamp & sign
-    -- rep.replace_image_common('/opt/DogUF/Docs/bill-fax-41260884.odt', '/var/lib/pgsql/fill-forms/signs-replica/imgStampКЭСББыков2.gif', 'img_stamp_sign');
-    SELECT stamp_n_sign INTO stamp_sign_file
-    FROM "Подписи"                                                               
-    WHERE "КодОтчета" = arg_templ -- 'СчетФакс'                                               
-          AND "НомерСотрудника" = 0                                              
-          AND "КлючФирмы" = our_firm
-    ORDER BY "ДатаСтартаПодписи" DESC LIMIT 1;
-    RAISE NOTICE 'stamp_sign_file=%', stamp_sign_file;
+    if arg_templ <> 'Счет' then  -- TODO оформить функцией с принятием решения
+        -- rep.replace_image_common('/opt/DogUF/Docs/bill-fax-41260884.odt', '/var/lib/pgsql/fill-forms/signs-replica/imgStampКЭСББыков2.gif', 'img_stamp_sign');
+        SELECT stamp_n_sign INTO stamp_sign_file
+        FROM "Подписи"                                                               
+        WHERE "КодОтчета" = arg_templ -- 'СчетФакс'                                               
+              AND "НомерСотрудника" = 0                                              
+              AND "КлючФирмы" = our_firm
+        ORDER BY "ДатаСтартаПодписи" DESC LIMIT 1;
+        RAISE NOTICE 'stamp_sign_file=%', stamp_sign_file;
 
-    if stamp_sign_file IS NOT NULL then
-        loc_res := rep.replace_image_common(format('%s/%s', out_dir, out_file), 
-                                                   format('%s/%s', templ_dir, stamp_sign_file), 'img_stamp_sign');
-        RAISE NOTICE 'SIGN replace_image_common loc_res=%', loc_res;
-        if loc_res <> '' then res := concat_ws(E'/', res, loc_res); end if;
-    end if;                                    
+        if stamp_sign_file IS NOT NULL then
+            loc_res := rep.replace_image_common(format('%s/%s', out_dir, out_file), 
+                                                       format('%s/%s', templ_dir, stamp_sign_file), 'img_stamp_sign');
+            if loc_res <> '' then
+                res := concat_ws(E'/', res, loc_res);
+                RAISE NOTICE 'SIGN replace_image_common loc_res=%', loc_res;
+            end if;
+        else
+            RAISE NOTICE 'stamp_sign_file is NULL for billno=%', arg_bill_no;
+        end if;
+    end if; -- не 'Счет'
 
 /**
 odt2pdf_query = "SELECT odt2pdf('" + outfile + "');"                             
@@ -125,11 +137,13 @@ res = plpy.execute(odt2pdf_query)
 #return outfile                                                                  
 return res[0]["odt2pdf"]                                                         
 **/
-    loc_res := odt2pdf(out_file, out_dir, out_dir);
-    RAISE NOTICE 'odt2pdf loc_res=%', loc_res;
+    if res = '' then -- не было ошибок
+        loc_res := odt2pdf(out_file, out_dir, out_dir);
+        RAISE NOTICE 'odt2pdf loc_res=%', loc_res;
+        res := loc_res;
+    end if;
 
     return res;
 END;
 $function$
 ;
-
