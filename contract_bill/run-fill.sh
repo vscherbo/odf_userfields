58834c70ee611f6e438a83154dcf9ec1b5a1f732
Владимир Щербо <vscherbo@gmail.com>
2024-06-20 17:28:43 +0300

    test script

--- contract_bill/run-fill.sh
+++ contract_bill/run-fill.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./fill_bill.py --pg_host=vm-pg-devel --pg_user=arc_energo --out_dir='/smb/system/Scripts/odf_userfields/Contracts/Docs/Bil/2020' --bill_no="$1"
+./fill_bill.py --pg_host=vm-pg-devel --pg_user=arc_energo --out_dir='/smb/system/Scripts/odf_userfields/Contracts/Docs' --bill_no="$1"
